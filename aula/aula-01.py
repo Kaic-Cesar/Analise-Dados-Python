d1bd1232d593d259a824fe18b93bf06c115807b5
Kaic Cesar <kaic_cesar@hotmail.com>
2024-06-06 10:10:31 -0300

    primeiro commit

---
+++ aula/aula-01.py
@@ -0,0 +1,16 @@
+# Aula 01 - Lógica de Programação 
+
+
+
+# Passo 1 - Percorrer todos os arquivos da paste base de dados (Pasta Vendas)
+
+# Passo 2 - Importar as bases de dados de vendas
+
+# Passo 3 - Tratar / Compilar as bases de dados 
+
+# Passo 4 - Calcular o produto mais vendido (em quantidade)
+
+# Passo 5 - Calcular o produto que mais faturou (em faturamento)
+
+# Passo 6 - Calcular a loja/cidade que mais vendeu (em faturamento) - Criar um gráfico/dashboard
+
