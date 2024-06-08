65f34bb35e1466dbadcbda2f322eec54d02fae85
Kaic Cesar <kaic_cesar@hotmail.com>
2024-06-07 21:09:11 -0300

    importando e tratando as bases dados

--- analise.py
+++ analise.py
@@ -2,21 +2,27 @@
 
 
 # Passo 1 - Percorrer todos os arquivos da paste base de dados (Pasta Vendas)
-import os 
-
+import os
+import pandas as pd # as pd (apelido) 
+ 
 local_arquivos = os.listdir("/Users/kaiccesar/Documents/Estudos/Python/Hashtag/Vendas") 
+tabela_total = pd.DataFrame()
+
 
 # Passo 2 - Importar as bases de dados de vendas
 for arquivo in local_arquivos: 
 
     if "vendas" in arquivo.lower():
-        print(f"/Users/kaiccesar/Documents/Estudos/Python/Hashtag/Vendas/{arquivo}")
+        # Importar arquivo
+        tabela = pd.read_csv(f"/Users/kaiccesar/Documents/Estudos/Python/Hashtag/Vendas/{arquivo}")
         # O "f" informa ao Python que esse texto aceita formatações dinâmicas 
 
 # Passo 3 - Tratar / Compilar as bases de dados
+        tabela_total = tabela_total._append(tabela) # Adiciona as tabelas para uma única tabela
 
+print(tabela_total)
 # Passo 4 - Calcular o produto mais vendido (em quantidade)
 
 # Passo 5 - Calcular o produto que mais faturou (em faturamento)
 
-# Passo 6 - Calcular a loja/cidade que mais vendeu (em faturamento) - Criar um gráfico/dashboard
+# Passo 6 - Calcular a loja/cidade que mais vendeu (em faturamento) - Criar um Gráfico/Dashboard
